6538be451be35ab7080c7e94e13416203d9641ce
dankito <dankito@user.noreply.github.com>
2016-11-27 00:12:40 +0100

    Implemented writing call list to and reading from file Implemented detection of missed calls Implemented updating call list when new calls have been retrieved

--- FritzBoxAndroid/src/main/java/net/dankito/fritzbox/services/CallListObserver.java
+++ FritzBoxAndroid/src/main/java/net/dankito/fritzbox/services/CallListObserver.java
@@ -6,11 +6,16 @@ import android.content.res.Resources;
 import net.dankito.fritzbox.R;
 import net.dankito.fritzbox.listener.CallListListener;
 import net.dankito.fritzbox.model.Call;
+import net.dankito.fritzbox.model.CallType;
 import net.dankito.fritzbox.model.UserSettings;
 import net.dankito.fritzbox.utils.web.callbacks.GetCallListCallback;
 import net.dankito.fritzbox.utils.web.responses.GetCallListResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -20,9 +25,14 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class CallListObserver {
 
+  protected static final String CALL_LIST_FILENAME = "CallList.json";
+
   protected static final String COULD_NOT_GET_CALL_LIST_NOTIFICATION_TAG = "CouldNotGetCallList";
 
   protected static final String MISSED_CALL_NOTIFICATION_TAG = "MissedCall";
+
+
+  private static final Logger log = LoggerFactory.getLogger(CallListObserver.class);
 
 
   protected Context context;
@@ -33,24 +43,29 @@ public class CallListObserver {
 
   protected NotificationsService notificationsService;
 
+  protected IFileStorageService fileStorageService;
+
   protected UserSettings userSettings;
+
+  protected List<Call> callList = new ArrayList<>();
 
   protected List<CallListListener> callListListeners = new CopyOnWriteArrayList<>();
 
 
-  public CallListObserver(Context context, FritzBoxClient fritzBoxClient, ICronService cronService, NotificationsService notificationsService, UserSettings userSettings) {
+  public CallListObserver(Context context, FritzBoxClient fritzBoxClient, ICronService cronService, NotificationsService notificationsService,
+                          IFileStorageService fileStorageService, UserSettings userSettings) {
     this.context = context;
     this.fritzBoxClient = fritzBoxClient;
     this.cronService = cronService;
     this.notificationsService = notificationsService;
+    this.fileStorageService = fileStorageService;
     this.userSettings = userSettings;
 
     if(userSettings.isPeriodicalMissedCallsCheckEnabled() && userSettings.getPeriodicalMissedCallsCheckInterval() > 0) {
       startPeriodicalMissedCallsCheck(cronService, userSettings.getPeriodicalMissedCallsCheckInterval());
     }
-    else { // if periodical check is disabled, get it at least at start up
-      getCallListAsync();
-    }
+
+    readStoredCallListAndThenUpdate();
   }
 
   protected void startPeriodicalMissedCallsCheck(ICronService cronService, long periodicalMissedCallsCheckInterval) {
@@ -63,6 +78,20 @@ public class CallListObserver {
       getCallListAsync();
     }
   };
+
+
+  protected void readStoredCallListAndThenUpdate() {
+    try {
+      this.callList = Arrays.asList(fileStorageService.readObjectFromFile(CALL_LIST_FILENAME, Call[].class));
+
+      callCallListUpdatedListeners(callList);
+    } catch(Exception e) {
+      log.error("Could not read call list from file " + CALL_LIST_FILENAME, e);
+    }
+
+    getCallListAsync();
+  }
+
 
   protected void getCallListAsync() {
     fritzBoxClient.getCallListAsync(new GetCallListCallback() {
@@ -87,30 +116,81 @@ public class CallListObserver {
   }
 
   protected void retrievedCallList(GetCallListResponse response) {
-    List<Call> missedCalls = getNewMissedCalls(response.getCallList());
+    List<Call> newlyRetrievedCalls = response.getCallList();
+    List<Call> missedCalls = getNewMissedCalls(newlyRetrievedCalls);
 
     if(missedCalls.size() > 0) {
-      Resources resources = context.getResources();
-      String title = resources.getString(R.string.notification_missed_call);
-      int iconId = resources.getIdentifier("@android:drawable/stat_notify_missed_call", null, null);
-
-      String missedCallsText = "";
-      for(Call missedCall : missedCalls) {
-        missedCallsText += missedCall.getCallerNumber() + "\n";
-      }
-
-      notificationsService.showNotification(title, missedCallsText, iconId, MISSED_CALL_NOTIFICATION_TAG);
+      showMissedCallsNotification(missedCalls);
     }
 
-    callCallListRetrievedListeners(response.getCallList());
+    handleCallListUpdate(response, newlyRetrievedCalls);
   }
 
-  protected List<Call> getNewMissedCalls(List<Call> callList) {
+  protected List<Call> getNewMissedCalls(List<Call> newlyRetrievedCalls) {
     List<Call> missedCalls = new ArrayList<>();
 
-    // TODO
+    if(this.callList.size() > 0) { // don't show all the missed calls on first app run
+      for (Call call : newlyRetrievedCalls) {
+        if(call.getType() == CallType.MISSED_CALL) {
+          if(this.callList.contains(call) == false) {
+            missedCalls.add(call);
+          }
+          else { // from now on callList only contains already known calls
+            break;
+          }
+        }
+      }
+    }
 
     return missedCalls;
+  }
+
+  protected void showMissedCallsNotification(List<Call> missedCalls) {
+    Resources resources = context.getResources();
+    String title = resources.getString(R.string.notification_missed_call);
+    int iconId = resources.getIdentifier("@android:drawable/stat_notify_missed_call", null, null);
+
+    String missedCallsText = "";
+    for(Call missedCall : missedCalls) {
+      missedCallsText += missedCall.getCallerNumber() + "\n";
+    }
+
+    notificationsService.showNotification(title, missedCallsText, iconId, MISSED_CALL_NOTIFICATION_TAG);
+  }
+
+  protected void handleCallListUpdate(GetCallListResponse response, List<Call> newlyRetrievedCalls) {
+    int countCallsBeforeMerge = callList.size();
+    this.callList = mergeWithNewlyRetrievedCalls(newlyRetrievedCalls);
+
+    if(callList.size() > countCallsBeforeMerge) { // call list updated
+      callCallListUpdatedListeners(response.getCallList());
+
+      try {
+        fileStorageService.writeObjectToFile(callList, CALL_LIST_FILENAME);
+      } catch(Exception e) {
+        log.error("Could not write call list to file " + CALL_LIST_FILENAME, e);
+      }
+    }
+  }
+
+  protected List<Call> mergeWithNewlyRetrievedCalls(List<Call> newlyRetrievedCalls) {
+    if(this.callList.size() == 0) {
+      this.callList = newlyRetrievedCalls;
+    }
+    else {
+      for (int i = 0; i < newlyRetrievedCalls.size(); i++) {
+        Call retrievedCall = newlyRetrievedCalls.get(i);
+
+        if(this.callList.contains(retrievedCall) == false) {
+          this.callList.add(i, retrievedCall);
+        }
+        else { // the other calls we know already
+          break;
+        }
+      }
+    }
+
+    return this.callList;
   }
 
 
@@ -122,10 +202,15 @@ public class CallListObserver {
     return callListListeners.remove(listener);
   }
 
-  protected void callCallListRetrievedListeners(List<Call> callList) {
+  protected void callCallListUpdatedListeners(List<Call> callList) {
     for(CallListListener listener : callListListeners) {
       listener.callListUpdated(callList);
     }
+  }
+
+
+  public List<Call> getCallList() {
+    return callList;
   }
 
 }
