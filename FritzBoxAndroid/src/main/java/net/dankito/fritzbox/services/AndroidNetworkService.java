06fba09b4aaec9fee6bfbaf1c5fb6f357f5ca807
dankito <dankito@user.noreply.github.com>
2017-02-18 17:25:00 +0100

    Implemented AndroidNetworkService

---
+++ FritzBoxAndroid/src/main/java/net/dankito/fritzbox/services/AndroidNetworkService.java
@@ -0,0 +1,37 @@
+package net.dankito.fritzbox.services;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+import static android.content.Context.WIFI_SERVICE;
+
+
+public class AndroidNetworkService implements INetworkService {
+
+  protected Context context;
+
+
+  public AndroidNetworkService(Context context) {
+    this.context = context;
+  }
+
+
+  @Override
+  public String getCurrentSsid() {
+    WifiManager wifiManager = (WifiManager) context.getSystemService(WIFI_SERVICE);
+    if(wifiManager != null && wifiManager.getConnectionInfo() != null) {
+      WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+      return wifiInfo.getSSID();
+    }
+
+    return null;
+  }
+
+  @Override
+  public boolean isInHomeNetwork(String homeNetworkSsid) {
+    String currentSsid = getCurrentSsid();
+    return currentSsid != null && currentSsid.equals(homeNetworkSsid);
+  }
+
+}
